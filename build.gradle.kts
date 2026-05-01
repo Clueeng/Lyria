c57da5b2605200410d5ec2cd61844f943f4ff67f
flaily <clueeng@gmail.com>
2026-05-01 10:40:47 +0200

    shaded in libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     kotlin("jvm") version "2.3.10"
     kotlin("plugin.serialization") version "2.3.10"
+//    id("com.gradleup.shadow") version "8.3.6"
+//    id("com.gradleup.shadow") version "8.1.1"
+//    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.5"
+    `maven-publish`
 }
 
 group = "cat.psychward.dbus"
@@ -22,6 +27,30 @@ dependencies {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+
+tasks.shadowJar {
+    // Relocate each dependency to avoid conflicts
+//    relocate("org.freedesktop.dbus", "cat.psychward.dbus.shaded.dbus")
+//    relocate("com.github.hypfvieh", "cat.psychward.dbus.shaded.hypfvieh")
+//    relocate("kotlinx.serialization", "cat.psychward.dbus.shaded.kotlinx.serialization")
+
+    mergeServiceFiles()
+    archiveClassifier.set("")
+
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("shadow") {
+            project.shadow.component(this)
+        }
+    }
 }
 
 tasks.test {
